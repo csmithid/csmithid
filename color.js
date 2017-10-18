3bc625155e616963eb73becead37522d7af9f730
Cole Smith <cole.smith@wework.com>
2017-10-18 11:09:08 -0400

    first draft title

---
+++ color.js
@@ -0,0 +1,3 @@
+var colors = ['#FA9645', '#69C88A', '#B02127', '#7783A6'];
+var random_color = colors[Math.floor(Math.random()*colors.length)];
+document.getElementById('colored').style.color = random_color;
