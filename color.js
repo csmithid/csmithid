1bfc8c8ef87d98eb3a819c02e89137c3ddca3bb6
Cole Smith <cole.smith@wework.com>
2018-10-19 13:29:09 -0400

    header and nav complete

--- color.js
+++
@@ -1,7 +0,0 @@
-var colors = ['DarkOrange','Gold','YellowGreen','DodgerBlue'];
-var random_color = colors[Math.floor(Math.random()*colors.length)];
-var modify = document.getElementsByClassName('colored');
-var i;
-for(i=0;i<modify.length;i++){
-  modify[i].style.color = random_color;
-}
